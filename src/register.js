c7208e321b6b0435c2a19e5a8c14ed794ec31aca
Alicia Flores <floresalvarado.alicia@gmail.com>
2021-02-20 12:45:30 -0600

    first login page

---
+++ src/register.js
@@ -0,0 +1,16 @@
+export const register = `
+<div class="div-register">
+  <img src="./assets/coffeehousesmall-01.png" alt="Logo Coffee House">
+  <h2> Registrate </h2>
+  <form>
+    <input type="email" class="input-email" placeholder="Correo Electrónico">
+    <input type="text" class="input-name" placeholder="Nombre completo">
+    <input type="text" class="input-user" placeholder="Nombre de usuario">
+    <input type="password" class="input-password" placeholder="Contraseña">
+    <input type="password" class="input-passwordConfirm" placeholder="Confirmar contraseña">
+    <input type="submit" class="submit-register" placeholder="Registrarme">
+  </form>
+  <p>Al registrarte aceptas términos y condiciones de privacidad</p>
+
+</div>
+`
