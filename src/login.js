d41d91963d83eeb880b09760d67b08e5cd8043a5
Alicia Flores <floresalvarado.alicia@gmail.com>
2021-02-22 13:16:18 -0600

    changes in routing

--- src/login.js
+++ src/login.js
@@ -1,29 +1,23 @@
 export const login = `
 <div class="div-login">
-  <img class="img-logo-login" src="./assets/coffeehousesmall-01.png" alt="Logo Coffee House">    
-  <h2> Inicia sesión </h2>
+  <img class="img-logo-login" src="./assets/coffeehousesmall-01.png" alt="Logo Coffee House">
+  <h2> Inicia sesión </h2><br>
   <form class="form-login">
-    <input class="input-login" type="email" class="input-email" placeholder="Correo Electrónico">
-    <input class="input-login" type="password" class="input-password" placeholder="Contraseña">
-    <input class="input-login" type="submit" class="submit-login" placeholder="Iniciar sesión">
-
+    <input class="input-login line" type="email" placeholder="Correo Electrónico">
+    <input class="input-login line" type="password" placeholder="Contraseña">
+    <button id="button-login" class="input-login send">Iniciar sesión</button>
   </form>
-
-  <p>¿No tienes una cuenta?</p>
-  <br><p>Registrate<a href="./register.js"><strong>AQUÍ</strong></a></p>
-  <p>
-    
-  <img class="leaves" src="./assets/LeafLF.png" alt="LeafLF">
-    
-  ó
-    
-  <img class="leaves" src="./assets/LeafRG.png" alt="LeafRG">
-  
-  </p>  
-  <p>Inicia Sesión con</p>
+  <div class="text-login">
+    <p>¿No tienes una cuenta?</p><br>
+    <p>Registrate <button id="hereRegister">AQUI</button></p>
+    <p><img class="leaves" src="./assets/LeafLF.png" alt="LeafLF">
+     ó
+     <img class="leaves" src="./assets/LeafRG.png" alt="LeafRG"></p>
+    <p>Inicia Sesión con</p>
+  </div>
   <div class="container-social">
     <img class="socialnetwork-logo" src="./assets/IconoFB.png">
     <img class="socialnetwork-logo" src="./assets/IconG+.png">
   </div>
 </div>
-`
+`;
