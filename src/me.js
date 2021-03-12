039af6a45fbc7fe1b48e9969931ac5c7f7dbec45
RaquelCFlores <cfloresraquel@gmail.com>
2021-03-12 16:47:41 -0600

    Mobile Only

--- src/me.js
+++ src/me.js
@@ -13,7 +13,7 @@ export const me = `
             <ul><li>Cafeterias cerca</li></ul>
             <ul><li>Ayuda y soporte técnico</li></ul>
             <ul><li>Configuracion</li></ul>
-            <ul><li><a class="logOut">Cerrar sesión</a></li></ul>
+            <ul><li><a class='logOut'>Cerrar sesión</a></li></ul>
         </nav>
     <img id='logo'class='logo-home' src='../assets/coffehouseletras-01.png' alt='Logo Coffee House'>
     <div class='div-search-head'>
@@ -81,104 +81,6 @@ export const me = `
 </div>
 `;
 
-// <div classs='principal-container' id='me-desktop'>
-//  <header class='header-desktop'>
-//    <div class='logo-desktop'>
-//      <img src='./assets/coffeehousesmall-01.png' alt='Logo'>
-//    </div>
-//    <div>
-//    <div class='container-navigation-desktop'>
-//      <img class='icon-home-desktop' src='../assets/calendar.png' alt='icon-muro' />
-//      <img class='icon-home-desktop' src='../assets/friends.png' alt='icon-friends' />
-//      <img class='icon-home-desktop' src='../assets/popular.png' alt='icon-popular' />
-//      <img class='icon-home-desktop' src='../assets/me.png' alt='icon-me' />
-//    </div>
-//    </div>
-//    <div class='container-search-desktop'>
-//      <form>
-//        <input class='search-desktop' type='text' placeholder='Buscar'/>
-//      </form>
-//    </div>
-//  </header>
-//  <main class='main-desktop'>
-//    <div class='menu-desktop'>
-//      <nav class='nav-desktop'>
-//        <ul><li>Angela Gomez</li></ul>
-//        <ul><li>Ver tu perfil</li></ul>
-//        <ul><li>Buscar amigos</li></ul>
-//        <ul><li>Guardado</li></ul>
-//        <ul><li>Cafeterias cerca</li></ul>
-//        <ul><li>Ayuda y soporte técnico</li></ul>
-//        <ul><li>Configuracion y Privacidad</li></ul>
-//        <ul><li><a class='logOut' >Cerrar sesión</a></li></ul>
-//      </nav>
-//      <div class='img-menu-desktop'><img src='../assets/pie Cafe.png' alt='coffee' /></div>
-//    </div>
-//    <div class='container-me-desktop'>
-//      <section class='container-posts-desktop'>
-//          <div class='div-photo-profile-desktop'>
-//              <img id='img-profile-desktop' src='../assets/withoutProfile.png'>
-//              <h3 id='name-user'> </h3>
-//          </div>
-//          <form class='my-posts-desktop' id='my-posts-desktop'>
-//              <input id='input-post-desktop' class='input-post-desktop' placeholder='Compartir algo'>
-//              <div class='icons-myPosts-desktop'>
-//                  <img class='here-icon-desktop icons-posts-desktop' src='../assets/here-icon.png'>
-//                  <img class='img-icon-desktop icons-posts-desktop' src='../assets/image-icon.png'>
-//                 <button id='send-icon-desktop' class='btn-icons-desktop'> <img class='send-icon-desktop icons-posts-desktop' src='../assets/send-icon.png'></button>
-//              </div>
-//          </form>
-//      <div class='container-img-desktop'>
-//          <figure class='img-post-desktop'><img src='../assets/image-uno.jpg'></figure>
-//          <figure class='img-post-desktop'><img src='../assets/image-dos.jpg'></figure>
-//          <figure class='img-post-desktop'><img src='../assets/image-tres.jpg'></figure>
-//      </div>
-//      <div class='container-icons-post-desktop'>
-//          <div class='icon-left-desktop'>
-//              <span class='counter-desktop' id='counter'>200</span>
-//              <button class='btn-icons-desktop'> <img id='icon-likes' class='icons-posts-desktop' src='../assets/Groupcuplike2.png'> </button>
-//              <button class='btn-icons-desktop'> <img id='icon-comments' class='icons-posts-desktop' src='../assets/comment-icon.png'> </button>
-//          </div>
-//          <div class='icon-right-desktop'>
-//              <button class='btn-icons-desktop'> <img id='icon-trash' class='icons-posts-desktop' src='../assets/trash-icon.png'> </button>
-//              <button class='btn-icons-desktop'> <img id='icon-edit' class='icons-posts-desktop' src='../assets/write-icon.png'> </button>
-//              <button class='btn-icons-desktop'> <img class='icons-posts-desktop' src='../assets/favorite-icon.png'> </button>
-//          </div>
-//      </div>
-//      <section class='section-posts-desktop'>
-//          <div class='myPost-desktop'>
-//          <p>Este es un comentario de mi post acerca de mi experiencia con algunas cafeterías de la ciudad en donde vivio.
-//          Mi lugar favorito para probar café en la cafetería Kayrom, que se encuentra a unos paso del centro historico</p>
-//          </div>
-//          <div class='div-comments-desktop'>
-//              <img class='photo-friend-desktop' src='../assets/withoutProfile.png'>
-//              <div class='first-comment-desktop'>
-//              </div>
-//          </div>
-//          <div class='div-comments-desktop'>
-//              <img class='photo-friend-desktop' src='../assets/withoutProfile.png'>
-//              <div class='second-comment-desktop'>
-//          </div>
-//          </div>
-//          <p class='counter-comments-desktop'>60 comentarios</p>
-//          <form class='form-comments-desktop'>
-//              <input class='post-comment-desktop' placheholder='Comenta algo'>
-//              <button id='send-comment-desktop' class='send-submit-desktop'> Enviar </button>
-//          </form>
-//      </section>
-//      </section>
-//    </div>
-//    <aside class='aside-desktop'>
-//      <figure class='adds-desktop'><img  src='./assets/add-uno.jpg' alt='anuncio' /></figure>
-//      <figcaption>Ir al sitio</figcaption>
-//      <figure class='adds-desktop'><img  src='./assets/add-dos.jpg' alt='anuncio' /></figure>
-//      <figcaption>Ir al sitio</figcaption>
-//      <figure class='adds-desktop'><img src='./assets/add-tres.jpg' alt='anun' /></figure>
-//      <figcaption>Ir al sitio</figcaption>
-//    </aside>
-//  </main>
-// </div>
-
 // --------Mobile--------- //
 
 let editStatus = false;
@@ -209,6 +111,7 @@ async function agregapost() {
         <div class='icon-right'>
             <button class='btn-icons icon-trash-mobile'><img data-id='${publicationMob.id}' class='icons-posts' src='../assets/trash-icon.png'></button>
             <button class='btn-icons icon-edit-mobile'> <img data-id='${publicationMob.id}' class='icons-posts' src='../assets/write-icon.png'> </button>
+            <button class='btn-icons'> <img id='icon-likes' class='icons-posts' src='../assets/Groupcuplike2.png'> </button>
         </div>
         `;
       const btnsTrashMob = document.querySelectorAll('.icon-trash-mobile');
@@ -264,83 +167,6 @@ document.addEventListener('submit', async (e) => {
   }
 });
 
-//---------Desktop------------//
-
-// let editStatusDesk = false;
-// let idDesk = '';
-// const savePostDesk = (inputPostDesk) => {
-//   dataBase.collection('posts').doc().set({
-//     inputPostDesk,
-//   });
-// };
-// const getPostDesk = (id) => dataBase.collection('posts').doc(id).get();
-// const onGetPostsDesk = (callback) =>
-//   dataBase.collection('posts').onSnapshot(callback);
-// const deletePostDesk = (id) => dataBase.collection('posts').doc(id).delete();
-// const updatePostDesk = (id, updatedPostDesk) =>
-//   dataBase.collection('posts').doc(id).update(updatedPostDesk);
-// document.addEventListener('DOMContentLoaded', async (e) => {
-//   const sectionPostDesk = document.getElementById('section-post-desktop');
-//   onGetPostsDesk((querySnapshot) => {
-//     sectionPostDesk.innerHTML = '';
-//     querySnapshot.forEach((doc) => {
-//       const publicationDesk = doc.data();
-//       publicationDesk.id = doc.id;
-//       sectionPostDesk.innerHTML += `<div class='myPost-desktop'>
-//        <p> ${publicationDesk.inputPostDesk}</p></div>
-//        <div class='icon-right-desktop'>
-//            <button class='btn-icons-desktop icon-trash-desktop'> <img data-id='${publicationDesk.id}' class='icons-posts-desktop' src='../assets/trash-icon.png'> </button>
-//            <button class='btn-icons-desktop icon-edit-desktop'> <img data-id='${publicationDesk.id}' class='icons-posts-desktop' src='../assets/write-icon.png'> </button>
-//        </div>
-//       `;
-//       const btnsTrashDesk = document.querySelectorAll('.icon-trash-desktop');
-//       btnsTrashDesk.forEach((btn) => {
-//         btn.addEventListener('click', async (e) => {
-//           await deletePostDesk(e.target.dataset.id);
-//         });
-//       });
-//       const myPostDesk = document.getElementById('my-posts-desktop');
-//       const btnsEditDesk = document.querySelectorAll('.icon-edit-desktop');
-//       btnsEditDesk.forEach((btn) => {
-//         btn.addEventListener('click', async (e) => {
-//           const docDesk = await getPostDesk(e.target.dataset.id);
-//           const postDesk = docDesk.data();
-//           editStatusDesk = true;
-//           idDesk = docDesk.id;
-//           myPostDesk['input-post-desktop'].value = postDesk.inputPostDesk;
-//           myPostDesk['send-icon-desktop'].innerHTML = `
-//               <button class='btn-icons-desktop'> <img id='icon-edit' class='icons-posts-desktop' src='../assets/write-icon.png'> </button>
-//               `;
-//         });
-//       });
-//     });
-//   });
-// });
-// document.addEventListener('submit', async (e) => {
-//   if (e.target.matches('#my-posts-desktop')) {
-//     const myPostDesk = document.getElementById('my-posts-desktop');
-//     e.preventDefault();
-//     const inputPostDesk = myPostDesk['input-post-desktop'];
-//     console.log(inputPostDesk);
-//     if (!editStatusDesk) {
-//       await savePostDesk(inputPostDesk.value);
-//     } else {
-//       await updatePostDesk(idDesk, {
-//         inputPostDesk: inputPostDesk.value,
-//       });
-//       editStatusDesk = false;
-//       idDesk = '';
-//       myPostDesk['send-icon-desktop'].innerHTML = `
-//            <button id='send-icon-desktop' class='btn-icons-desktop'> <img class='send-icon-desktop icons-posts-desktop' src='../assets/send-icon.png'></button>
-//           `;
-//     }
-//     myPostDesk.reset();
-//     inputPostDesk.focus();
-//   }
-// });
-
-
-
 function showMenu(){
     const menu = document.getElementById('nav-mobile');
     if(menu.classList.contains('disabled-menu')){
@@ -371,6 +197,9 @@ document.addEventListener('click', (e) => {
     console.log('Burger');
     showMenu();
     e.preventDefault();
+  }
+  if(e.target.matches('#icon-likes')) {
+    console.log('Me gusta');
   }
 });
 
